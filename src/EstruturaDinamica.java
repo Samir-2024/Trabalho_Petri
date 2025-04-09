ed098b073a450c2361db0c81a7c9f9e206b7493e
Samir-Chehade <samir.chehade42@gmail.com>
2025-04-09 14:06:41 -0300

    Update EstruturaDinamica.java

--- src/EstruturaDinamica.java
+++ src/EstruturaDinamica.java
@@ -2,12 +2,58 @@ public class EstruturaDinamica {
     private No primeiroElemento;
     private No ultimoElemento;
     private int qtdElementos;
-    private boolean cheia;
 
     public EstruturaDinamica() {
         this.primeiroElemento = null;
         this.ultimoElemento = null;
         this.qtdElementos = 0;
-        this.cheia = false;
+    }
+
+    public void inserirElemento(No elemento) {
+        No novoNo = new No(elemento);
+        if (estaVazia()) {
+            primeiroElemento = novoNo;
+            ultimoElemento = novoNo;
+        } else {
+            ultimoElemento.setProximoNo(novoNo);
+            novoNo.setAnteriorNo(novoNo);
+            ultimoElemento = novoNo;
+        }
+        qtdElementos++;
+
+    }
+
+    public boolean estaVazia() {
+       if (qtdElementos == 0) {
+            System.out.println("A lista contém elementos");
+            return true;
+       } else {
+            System.out.println("A lista está vazia");
+            return false;
+       }
+
+    }
+
+    public boolean estaCheia() {
+        if (qtdElementos > 0) {
+            return true;
+
+        } else {
+            return estaVazia();
+        }
+    }
+
+    public void imprimirLista() {
+        No atual = primeiroElemento;
+        while (atual != null) {
+            System.out.println(atual.getElemento());
+            atual = atual.getProximoNo();
+        }
+    }
+
+    public void removerElemento(Object elemento) {
+        if(estaVazia()) {
+            System.out.println("A lista está vazia");
+        }
     }
 }
